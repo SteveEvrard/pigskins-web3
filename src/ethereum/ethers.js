998cf4ad5649c77d2bac1a8119cfdf4d05544f8c
SteveEvrard <stevelevrard@gmail.com>
2021-10-22 14:23:18 -0400

    Update contract addresses

--- src/ethereum/ethers.js
+++ src/ethereum/ethers.js
@@ -4,8 +4,8 @@ import CardGame from './abi/CardGameAbi.json';
 
 export const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-export const cardAuctionContractAddress = '0x58c0CD76d74782ddb254BBc7b7eb07331B6Bf3A3';
-export const cardGameContractAddress = '0x5DBF1933b414E627Ee55eDe2763e7Cf2F1E01aF0';
+export const cardAuctionContractAddress = '0x45c3796E1730bFf4EC49C087B0C09AAA3183D85c';
+export const cardGameContractAddress = '0xfE4EB2797e290b4956e9a48cba5169101bd7195A';
 
 export const signer = provider.getSigner();
 
